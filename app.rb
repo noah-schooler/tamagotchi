009df0a37cbcf3b4cb4cea53146006ee82559d13
Noah Schooler and Phil Mass <noah@>
2018-11-28 15:29:58 -0800

    create file structure

--- app.rb
+++ app.rb
@@ -3,31 +3,3 @@ require('sinatra/reloader')
 require('./lib/place.rb')
 also_reload('lib/**/*.rb')
 require('pry')
-
-get('/') do
-  @places = Place.all()
-  erb(:index)
-end
-
-post('/') do
-  if(params['rank'])
-    place = Place.find(params["id"].to_i)
-    rank = params['rank'].to_i
-    Place.order(place, rank)
-  elsif(params['city'])
-    city = params['city']
-    state = params['state']
-    description = params['description']
-    place = Place.new(city, state)
-    place.description = description
-    place.save()
-  end
-  @places = Place.all()
-  erb(:index)
-end
-
-get('/cities/:id') do
-  @place = Place.find(params[:id].to_i)
-  @length = Place.all().length
-  erb(:city)
-end
---
+++ spec/tamagotchi_spec.rb
@@ -0,0 +1,3 @@
+require ('rspec')
+require('place')
+require ('pry')

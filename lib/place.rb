a9e5732d305cb028f7e407c3ed4a5c8862d967ba
Noah Schooler and Phil Mass <noah@>
2018-11-28 09:44:06 -0800

    add failing id test

--- lib/place.rb
+++ lib/place.rb
@@ -1,15 +1,24 @@
 class Place
   @@list = []
+  @@next_id = 0
+
+  def self.clear
+    @@list = []
+  end
 
   def self.all()
     @@list
+  end
+
+  def initialize()
+    @id = @@next_id
+    @@next_id += 1
   end
 
   def save
     @@list.push(self)
   end
 
-  def self.clear
-    @@list = []
+  def id
   end
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -13,14 +13,6 @@ describe("Place") do
     end
   end
 
-  describe("#save") do
-    it("saves new place to class array") do
-      place = Place.new()
-      place.save()
-      expect(Place.all()).to(eq([place]))
-    end
-  end
-
   describe(".clear") do
     it("clears the list array") do
       place = Place.new()
@@ -29,4 +21,29 @@ describe("Place") do
       expect(Place.all()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("saves new place to class array") do
+      place = Place.new()
+      place.save()
+      expect(Place.all()).to(eq([place]))
+    end
+  end
+
+  describe("#id") do
+    it("adds a unique id to each place starting at 0") do
+      place = Place.new()
+      place.save()
+      expect(place.id()).to(eq(0))
+    end
+  end
+  # describe("#find") do
+  #   it("finds a place in the list array") do
+  #     place0 = Place.new()
+  #     place0.save()
+  #     place1 = Place.new()
+  #     place1.save()
+  #     expect(Place.find())
+  #   end
+  # end
 end
